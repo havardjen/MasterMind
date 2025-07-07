bb654b1cb999f0cca2cb5411f28d0f6950f52d75
Jensås, Håvard Østgård <haavard.ostgaard.jensaas@arbeidstilsynet.no>
2025-07-07 11:08:21 +0200

    Adding new API with one get, fixing gitignore

---
+++ MasterMindAPI/Controllers/MasterMindController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MasterMindAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MasterMindController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok("Welcome to this super incredible amazing relatively ok MasterMind service");
+        }
+    }
+}
